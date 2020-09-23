f2aa6e7edffae66907c0d3f8059f64a5617cf59c
Marcelo <magalhaesmarcelo@outlook.com.br>
2020-09-23 17:23:22 -0300

    Resolving challenge.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,11 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface CreateTransactionInterface {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +19,32 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (acc, cur) => {
+        if (cur.type === 'income') acc.income += cur.value;
+        else if (cur.type === 'outcome') acc.outcome += cur.value;
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+      },
+    );
+    return { income, outcome, total: income - outcome };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({
+    title,
+    type,
+    value,
+  }: CreateTransactionInterface): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
